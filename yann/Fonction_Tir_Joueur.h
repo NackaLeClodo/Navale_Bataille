7d7b07c85da8490417a6955f1ece10559b1fa486
vorozz <yannou28300@gmail.com>
2021-11-09 16:37:43 +0100

    NEW

---
+++ yann/Fonction_Tir_Joueur.h
@@ -0,0 +1,12 @@
+#ifndef FONCTION_TIR_JOUEUR_H
+#define FONCTION_TIR_JOUEUR_H
+
+#endif // FONCTION_TIR_JOUEUR_H
+
+void main ()
+
+    int placer;
+    int afficher;
+    int jouer;
+    int sauv;
+    int charger;
---
+++ yann/Navale_Bataille_Yann.pro
@@ -0,0 +1,10 @@
+TEMPLATE = app
+CONFIG += console c++11
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+    Fonction_Tir_Joueur.cpp
+
+HEADERS += \
+    Fonction_Tir_Joueur.h
